94b9fdf504a2d1a0b57dea110e9f527bd55b5846
Dermot <contact@dermotobrien.me>
2021-04-19 20:36:53 +0100

    Use ffmpeg to take screenshot

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <cstdio>
+#include <ctime>
 
 using namespace std;
 
 int main() 
 {
     cout << "Screen capture and upload to Mega encrypted storage provider!\n";
+
+    std::time_t timestamp = std::time(0);
+    std::string fileName  = std::to_string(timestamp) + ".png";
+
+    std::string directory = "/images/" + fileName;
+    
+    // cout << directory;
+
+    string command = "cd images && ffmpeg -f x11grab -framerate 1 -video_size 1920x1080 -i :0.0 -vframes 1 " + fileName + " && cd ../";
+    char* takeScreenshot = &command[0];
+
+    system(takeScreenshot);
+
+
     return 0;
 }
+
