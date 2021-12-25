1663124926e1dc2d8f179937892b09bdf1bb2250
Dermot <contact@dermotobrien.me>
2021-12-25 00:46:38 +0000

    Implement Electron application with Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Svelte ElectronJS application is working!</p>
 </main>
 
 <style>
