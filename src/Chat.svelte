bae07af898ab1bf77c9ca2f5565fd0c8402979f1
Dermot <contact@dermotobrien.me>
2022-01-02 16:46:36 +0000

    Create Chat.svelte

---
+++ src/Chat.svelte
@@ -0,0 +1,48 @@
+<script>
+  import GUN from "gun";
+  const gun = new GUN();
+
+//   import { user, userData } from "./User";
+
+//   console.log({ user, userData });
+
+  let username = "Mark";
+
+  //   if(username) startGun();
+
+  // on button click, set username
+  function setUsername() {
+    console.log({ msg: "updating username", username });
+    startGun();
+  }
+
+  function startGun() {
+    console.log("starting gun");
+    gun.get(username).put({
+      name: "Mark",
+      email: "mark@gun.eco",
+    });
+
+    gun.get(username).on(async (data, key) => {
+      console.log("realtime updates:", data);
+      console.log({ key });
+      let account = await gun.get(username).get("account");
+      console.log("account", account);
+    });
+
+    setInterval(() => {
+      let index = new Date().toISOString();
+      gun
+        .get(username)
+        .get("account")
+        .get(index)
+        .put({ details: Math.random() });
+    }, 4000);
+  }
+</script>
+
+<div class="container">
+  <h1>Chat loaded</h1>
+  <input type="text" id="username" bind:value={username} />
+  <button on:click={setUsername}>Send</button>
+</div>
