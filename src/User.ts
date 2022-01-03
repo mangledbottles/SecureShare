8bc3c6b84ee1d09648a41ecb54e332bdc61b5cda
Dermot <contact@dermotobrien.me>
2022-01-03 23:12:07 +0000

    Store username through application and monitor auth

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,6 @@
 import GUN from 'gun';
 import 'gun/sea';
-import 'gun/axe';
+// import 'gun/axe';
 import { writable } from 'svelte/store';
 
 // Database
@@ -9,9 +9,14 @@ export const db = GUN();
 // Gun User
 export const user = db.user().recall({ sessionStorage: true });
 
-// User Data
-export const userData = writable({});
+// Current User's username
+export const username = writable('');
 
-user.get('alias').once(alias => {
-  userData.set({ alias });
+user.get('alias').on(v => username.set(v))
+
+db.on('auth', async (event) => {
+  const alias = await user.get('alias'); // username string
+  username.set(alias.toString());
+
+  console.log(`signed in as ${alias}`);
 });
