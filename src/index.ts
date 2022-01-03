296f92d254fc91edeff454bde65b150cf2498eac
Dermot <contact@dermotobrien.me>
2022-01-03 19:00:22 +0000

    Apply electron-reload to Svelte app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, '../node_modules', '.bin', 'electron'),
+  awaitWriteFinish: true,
+});
+
 app.on("ready", () => {
   const mainWindow = new BrowserWindow();
   mainWindow.loadFile(path.join(__dirname, "../public/index.html"));
