79e79fde5e39863cf647d06bccf2f6ac52a163ed
Dermot <contact@dermotobrien.me>
2022-01-03 23:12:28 +0000

    Conditionally render Authenticated content

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,39 @@
 <script lang="ts">
-	export let name: string;
-	import Chat from "./Chat.svelte";
+  export let name: string;
+  import { username, user } from "./User";
+  import Header from "./Header.svelte";
+  import Chat from "./Chat.svelte";
+  import Auth from "./Auth.svelte";
 </script>
 
 <main>
-	<h1>{name}</h1>
-	<Chat />
+  <Header name={name} />
+  {#if $username}
+    <Chat />
+  {/if}
+  {#if !$username}
+    <Auth />
+  {/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
