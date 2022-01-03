ecf686cef9dc9d618c60c3055bea5c1fd832faea
Dermot <contact@dermotobrien.me>
2022-01-03 23:10:38 +0000

    Create Login and Signup forms
    TODO: clean up UI

---
+++ src/Auth.svelte
@@ -0,0 +1,8 @@
+<label for="username">Username</label>
+<input name="username" bind:value={username} minlength="3" maxlength="32" />
+
+<label for="password">Password</label>
+<input name="password" bind:value={password} type="password" />
+
+<button class="login" on:click={login}>Login</button>
+<button on:click={signup}>Sign Up</button>
