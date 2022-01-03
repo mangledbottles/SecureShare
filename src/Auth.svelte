ca4fbc570fdd1d2f99467dd4c17573ef234face6
Dermot <contact@dermotobrien.me>
2022-01-03 23:11:17 +0000

    Import dependencies

--- src/Auth.svelte
+++ src/Auth.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+  import { user } from "./User";
+
+  // Import dependencies
+  import randomWords from "random-words";
+  import { v4 as uuidv4 } from "uuid";
+
+  let username, password;
+</script>
+
 <label for="username">Username</label>
 <input name="username" bind:value={username} minlength="3" maxlength="32" />
 
