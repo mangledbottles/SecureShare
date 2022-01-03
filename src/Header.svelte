8c1b7f7af4f7d9b312ca944c71b27a6b6fdd593a
Dermot <contact@dermotobrien.me>
2022-01-03 23:12:13 +0000

    Create Header.svelte

---
+++ src/Header.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { username, user } from "./User";
+  export let name: string;
+  function signout() {
+    console.log("signing out");
+    user.leave();
+    username.set("");
+  }
+
+  // Import dependencies
+  import { PopoverPositions } from "attractions/popover";
+  import { Popover, Button, PopoverButton } from "attractions";
+  import Clipboard from "svelte-clipboard";
+  //   import { clipboard } from 'electron';
+
+  //   function copyUsername() {
+  //       console.log("copying username");
+  //     this.clipboard.writeText(username);
+  // clipboard.writeText(username);
+  //   }
+</script>
+
+<header>
+  <h1>{name}</h1>
+  {#if $username}
+    <div class="user-bio">
+      <span>Hello <strong>{$username}</strong></span>
+
+      <Popover position={PopoverPositions.BOTTOM}>
+        <Clipboard
+          text={$username}
+          let:copy
+          on:copy={() => {
+            console.log("copied");
+          }}
+        >
+          <Button on:click={copy}>Username: {$username}</Button>
+          <div slot="popover-content">
+            <PopoverButton>copy</PopoverButton>
+          </div>
+        </Clipboard>
+      </Popover>
+
+      <!-- <img
+        src={`https://avatars.dicebear.com/api/initials/${$username}.svg`}
+        alt="avatar"
+      /> -->
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+  {:else}
+    <h3>Gun.js Chat</h3>
+  {/if}
+</header>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
